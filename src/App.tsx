b3c9cfc3365bb0ffb3287b875377efb6b8aeffc5
Luiz Oliveira <luizoliveira2808@gmail.com>
2025-02-12 11:57:20 -0300

    feat: add dark mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
+import { ModeToggle } from "./components/layout-components/mode-toggle";
+import { ThemeProvider } from "./components/provider-components/theme-provider";
 import { Button } from "./components/ui/button";
 
 export function App() {
   return (
-    <div className="flex flex-col items-start gap-2 p-4 pt-2">
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-      <Button>Click me</Button>
-    </div>
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <div className="flex flex-col items-start gap-2 p-4 pt-2">
+        <h1 className="text-3xl font-bold underline">Hello world!</h1>
+        <Button>Click me</Button>
+        <ModeToggle />
+      </div>
+    </ThemeProvider>
   );
 }
