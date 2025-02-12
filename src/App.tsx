6aa78d1aa8a21fda7a949e8d181229a81b1e9c0a
Luiz Oliveira <luizoliveira2808@gmail.com>
2025-02-12 11:45:26 -0300

    chore: add shadcn/ui library

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+import { Button } from "./components/ui/button";
+
 export function App() {
   return (
-    <div className="p-4 pt-2">
+    <div className="flex flex-col items-start gap-2 p-4 pt-2">
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <Button>Click me</Button>
     </div>
   );
 }
